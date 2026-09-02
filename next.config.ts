a9c04a32d91b0ee1bb8365e8dec541a1974bd4ae
huydepzai2372 <your_email@gmail.com>
2026-09-03 00:27:49 +0700

    add production code obfuscation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
+import JavaScriptObfuscator from "webpack-obfuscator";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  productionBrowserSourceMaps: false,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.plugins.push(
+        new JavaScriptObfuscator({
+          compact: true,
+          controlFlowFlattening: true,
+          controlFlowFlatteningThreshold: 0.75,
+          deadCodeInjection: false,
+          disableConsoleOutput: true,
+          identifierNamesGenerator: "hexadecimal",
+          stringArray: true,
+          stringArrayEncoding: ["base64"],
+          stringArrayThreshold: 0.75,
+        }),
+      );
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
